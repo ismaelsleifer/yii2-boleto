653d1e3fad8cfd3f9479bdace0afe1843c69d4b0
Ismael Sleifer <ismaelsleifer@gmail.com>
2022-09-18 23:54:35 -0300

    ajustes

--- Convenio.php
+++ Convenio.php
@@ -2,21 +2,34 @@
 
 namespace sleifer\boleto;
 
-use yii\helpers\VarDumper;
+use common\components\Number;
+use Picqer\Barcode\BarcodeGeneratorHTML;
 
 class Convenio{
 
     private $bar_code;
 
     public function setBarCode($barCode){
-
-        $this->bar_code = $barCode;
+        $bar = str_replace(['.', '-', ' '], '', $barCode);
+        $b1 = substr($bar,  0, 11);
+        $b2 = substr($bar, 12, 11);
+        $b3 = substr($bar, 24, 11);
+        $b4 = substr($bar, 36, 11);
+        $this->bar_code = $b1.$b2.$b3.$b4;
     }
 
-    public function getValue(){
+    public function getValorDocumento(){
+        $value = ltrim(substr($this->bar_code, 4, 11), '0'); 
+        return Number::numberFormat(substr_replace($value, '.', -2, 0));
+    }
 
-        VarDumper::dump($this->bar_code, 10, true); die(__FILE__ . ' - ' . __LINE__);
+    public function desenhaBarras(){
+        $barCode = new BarcodeGeneratorHTML();
+        return $barCode->getBarcode($this->bar_code, $barCode::TYPE_INTERLEAVED_2_5, 1, 50);;
+    }
 
+    public function getDtVencimento(){
+        return;
     }
     
 }
